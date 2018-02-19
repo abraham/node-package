0ed32efca45547fbc11048fea9ce08bb1c85d4da
Abraham Williams <4braham@gmail.com>
2018-02-19 12:43:58 -0600

    Unpkg URL should be wrapped in a script tag

--- test/node-package.test.ts
+++ test/node-package.test.ts
@@ -113,7 +113,7 @@ describe('<node-package>', () => {
         assertCommand(1, 'git clone git://github.com/petkaantonov/bluebird.git');
         tabs[2].click();
         await sleep(TIMEOUT);
-        assertCommand(2, 'https://unpkg.com/bluebird/js/browser/bluebird.js');
+        assertCommand(2, '<script async src="https://unpkg.com/bluebird/js/browser/bluebird.js"></script>');
       });
 
       describe('without web or git', () => {
